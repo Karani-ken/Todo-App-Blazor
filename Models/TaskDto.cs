f25fff5292cbcb28eaade05f5466208ec092648d
karani-ken <academice79@gmail.com>
2023-09-12 21:44:30 +0300

    Add project files.

---
+++ Models/TaskDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Todo_App_Blazor.Models
+{
+    public class TaskDto
+    {
+        public Guid TaskId{ get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
