daf68b193b7cb91883f8286347786eeeebe83865
karani-ken <academice79@gmail.com>
2023-09-14 22:41:07 +0300

    Added Blazor Toastify

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+//using Blazored.Toast;
+using BlazorToastify;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Todo_App_Blazor;
@@ -7,5 +9,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+//builder.Services.AddBlazoredToast();
+builder.Services.AddToasts();
 await builder.Build().RunAsync();
